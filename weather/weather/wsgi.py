a39cbbff44b13e4671c8985e9f362805f92a8082
ajita singh <ajitavsingh_8@yahoo.com>
2017-11-16 13:33:20 -0500

    another commit

--- weather/weather/wsgi.py
+++ weather/weather/wsgi.py
@@ -17,6 +17,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather.settings")
 application = get_wsgi_application()
 
 
-path = '/Users/manishsinha/coding-django/weather'
-if path not in sys.path:
-    sys.path.append(path)
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
